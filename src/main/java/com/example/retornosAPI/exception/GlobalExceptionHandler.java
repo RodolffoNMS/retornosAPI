3ecd9d229e296945c2804a9dee5947a48d83afc5
RodolffoNMS <rodolffo.souza@zup.com.br>
2025-01-27 15:06:53 -0300

    feat: Lidando com a classe ExceptionHandler

--- src/main/java/com/example/retornosAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/retornosAPI/exception/GlobalExceptionHandler.java
@@ -1,53 +1,30 @@
 package com.example.retornosAPI.exception;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception ex, WebRequest request) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", "Internal Server Error");
-        body.put("message", ex.getMessage());
-        body.put("path", request.getDescription(false).replace("uri=", ""));
 
-        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocorreu um erro. Eu não sei o que, não sei onde. Só sei que deu erro. Volta mais tarde ae, quem sabe resolveu, ou talvez não...");
+
     }
 
-    @ExceptionHandler(RuntimeException .class)
-    public ResponseEntity<Map<String, Object>> handleProductNotFoundException(RuntimeException  ex, WebRequest request) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("timestamp", LocalDateTime.now());
-        map.put("status", HttpStatus.NOT_FOUND.value());
-        map.put("error", "Not Found");
-        map.put("message", ex.getMessage());
-        map.put("path", request.getDescription(false).replace("uri=", ""));
-
-        return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Algo de errado não está certo: " + ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationException(IllegalArgumentException ex, WebRequest request) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("timestamp", LocalDateTime.now());
-        map.put("status", HttpStatus.BAD_REQUEST.value());
-        map.put("error", "Bad Request");
-        map.put("message", ex.getMessage());
-        map.put("path", request.getDescription(false).replace("uri=", ""));
-
-        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleValidationException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Erro de validação: " + ex.getMessage());
     }
 }
-
